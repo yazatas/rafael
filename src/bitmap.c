d3b71e799083e59a6b5aeef423a5329dc408a048
rficu <rficu@airmail.cc>
2018-04-10 20:28:56 +0300

    update bitmap code

--- src/bitmap.c
+++ src/bitmap.c
@@ -1,11 +1,12 @@
 #include "bitmap.h"
+#include "debug.h"
 #include <stdio.h>
 
 /* set nth bit */
 int bm_set_bit(bitmap_t *bm, uint32_t n)
 {
 	if (n / 32 > bm->len) {
-		debug("bit %u is over range!", n);
+		debug(LOG_EMERG, "bit %u is over range!", n);
 		return -1;
 	}
 
@@ -13,12 +14,10 @@ int bm_set_bit(bitmap_t *bm, uint32_t n)
 	return 0;
 }
 
-// TODO what is k
-// TODO what is n
 int bm_set_range(bitmap_t *bm, uint32_t n, uint32_t k)
 {
 	if (n / 32 > bm->len || k / 32 > bm->len) {
-		debug("argument is over range!");
+		debug(LOG_EMERG, "argument is over range!");
 		return -1;
 	}
 
@@ -34,7 +33,7 @@ int bm_set_range(bitmap_t *bm, uint32_t n, uint32_t k)
 int bm_unset_bit(bitmap_t *bm, uint32_t n)
 {
 	if (n / 32 > bm->len) {
-		debug("bit %u is over range!", n);
+		debug(LOG_EMERG, "bit %u is over range!", n);
 		return -1;
 	}
 
@@ -45,7 +44,7 @@ int bm_unset_bit(bitmap_t *bm, uint32_t n)
 int bm_unset_range(bitmap_t *bm, uint32_t n, uint32_t k)
 {
 	if (n / 32 > bm->len || k / 32 > bm->len) {
-		debug("argument is over range!");
+		debug(LOG_EMERG, "argument is over range!");
 		return -1;
 	}
 
@@ -57,10 +56,10 @@ int bm_unset_range(bitmap_t *bm, uint32_t n, uint32_t k)
 }
 
 /* return -1 on error and 0/1 on success */
-int test_bit(bitmap_t *bm, uint32_t n)
+int bm_test_bit(bitmap_t *bm, uint32_t n)
 {
 	if (n / 32 > bm->len) {
-		debug("bit %u is over range!", n);
+		debug(LOG_EMERG, "bit %u is over range!", n);
 		return -1;
 	}
 	return (bm->bits[n / 32] & (1 << (n % 32)));
--- src/bitmap.h
+++ src/bitmap.h
@@ -9,14 +9,16 @@ typedef struct bitmap {
 	uint32_t *bits;
 } bitmap_t;
 
-// TODO documentation ASAP!!!!
+
+// TODO create (de)allocation api for bitmap
 
 int bm_set_bit(bitmap_t *bm, uint32_t n);
-int bm_set_range(bitmap_t *bm, uint32_t n, uint32_t k);
-
 int bm_unset_bit(bitmap_t *bm, uint32_t n);
+
+/* set values from n to k to 1/0 */
+int bm_set_range(bitmap_t *bm, uint32_t n, uint32_t k);
 int bm_unset_range(bitmap_t *bm, uint32_t n, uint32_t k);
 
-int test_bit(bitmap_t *bm, uint32_t n);
+int bm_test_bit(bitmap_t *bm, uint32_t n);
 
 #endif /* end of include guard __BITMAP_H__ */
