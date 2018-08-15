8c81c7707fe8f1a11fa45a81ab01a38f13694e17
rficu <rficu@airmail.cc>
2018-08-15 18:10:14 +0300

    update fs errors

--- src/fs_errno.c
+++ src/fs_errno.c
@@ -3,11 +3,26 @@
 // TODO add mutex to guard this
 int fs_errno;
 
-const char *fs_strerror(int fs_errno)
+static int fs_errno_new;
+
+void fs_set_errno(fs_status_t status)
 {
-	switch (fs_errno) {
-		case FS_FAIL_INVALID_MAGIC_NUMBER: return "Invalid magic number";
-		case FS_FAIL_SUPERBLOCK_NOT_FOUND: return "Superblock not found";
+    /* TODO: use pthread for now?? */
+    fs_errno_new = status;
+}
+
+const char *fs_strerror(fs_status_t fs_errno)
+{
+	switch (fs_errno_new) {
+
+        case FS_OK:                        return "Success";
+        case FS_READ_FAILED:               return "Read failed";
+        case FS_WRITE_FAILED:              return "Write failed";
+        case FS_NOT_ENOUGH_SPACE:          return "Not enough space";
+        case FS_SB_READ_FAILED:            return "Failed to read Superblock";
+        case FS_SB_INVALID_MAGIC:          return "Invalid magic number";
+
+        /* FIXME: deprecated, remove */
 		case FS_FAIL_NOT_ENOUGH_SPACE:     return "Not enough space on the device";
 		case FS_FAIL_READ_FAILED:          return "Unable to read from device";
 		case FS_FAIL_WRITE_FAILED:         return "Unable to write to device";
--- src/fs_errno.h
+++ src/fs_errno.h
@@ -1,30 +1,25 @@
 #ifndef __FS_ERRNO_H__
 #define __FS_ERRNO_H__
 
+/* TODO: make it static and create fs_set_errno 
+ * which uses a mutex to guard the errno */
 extern int fs_errno;
 
-/* How to use:
- *
- * 1) add new error code to FS_ERRORS
- *   -> style: FS_<ACTION>_FAIL_<CONDITION>
- *      -> FS_MOUNT_FAIL_INVALID_SUPERBLOCK
- *      -> FS_MKFS_FAIL_NOT_ENOUGH_SPACE
- * 2) add explanation of error to fs_strerror
- *    -> only a general explanation, function that failed has
- *       written better explanation to error logs
- */
-
-enum fs_errno_t {
+typedef enum fs_status {
 	FS_OK,
-	FS_FAIL_INVALID_MAGIC_NUMBER,
-	FS_FAIL_INVALID_SUPERBLOCK,
-	FS_FAIL_SUPERBLOCK_NOT_FOUND,
+    FS_READ_FAILED,
+    FS_WRITE_FAILED,
+    FS_NOT_ENOUGH_SPACE,
+    FS_SB_READ_FAILED,
+    FS_SB_INVALID_MAGIC,
+
 	FS_FAIL_NOT_ENOUGH_SPACE,
 	FS_FAIL_READ_FAILED,
 	FS_FAIL_WRITE_FAILED,
 	FS_FAIL_DIRTY_FLAG_SET,
-};
+} fs_status_t;
 
-const char *fs_strerror(int fs_errno);
+const char *fs_strerror(fs_status_t status);
+void fs_set_errno(fs_status_t status);
 
 #endif /* end of include guard: __FS_ERRNO_H__ */
